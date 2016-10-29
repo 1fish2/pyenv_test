b84d4800987624c27d8b7f277f67de6af2229be3
Jerry Morrison <1fish2@users.noreply.github.com>
2016-10-28 21:07:04 -0700

    Add the traceback; refine the comments

--- setup.py
+++ setup.py
@@ -1,33 +1,56 @@
 """
 0. Background, if not already done on this computer:
-    * Install pyenv, pyenv-virtualenv, pyenv-virtualenvwrapper
+    brew install pyenv
+    brew install pyenv-virtualenv
+    brew install pyenv-virtualenvwrapper
 
-1. Create a Python virtualenv like this:
+1. Create a Python virtualenv:
     CONFIGURE_OPTS="--enable-shared" pyenv install 2.7.10   # Install Python 2.7.10
     pyenv local 2.7.10        # Use 2.7.10 in the local directory
     pip install --upgrade pip setuptools
+    pyenv rehash
     pyenv virtualenv sum      # Create the "sum" virtual environment using 2.7.10 as its base
     pyenv local sum           # Use the "sum" virtual environment in the local directory
-    pip install numpy==1.9.2 Cython
+    pip install --no-binary :all: numpy==1.9.2 Cython   # Problematic w/o "--no-binary :all:"
     pyenv rehash
 
-2. Build the Cython code like this:
+2. Build the Cython code:
     rm -f sum.so sum.c; python setup.py build_ext --inplace
 
-3. Then run the test case like this:
+3. If you get this far, run the test case:
     python testenv.py
+
+4. Try installing nose or line_profiler, rehashing, and running the test
+under the installed executable nosetest or kernprof.
 """
 
 from distutils.core import setup
 from Cython.Build import cythonize
-
 import numpy as np
-import os
 
 sum_module = cythonize("sum.pyx")
+setup(name="Sum", ext_modules=sum_module, include_dirs=[np.get_include()])
 
-setup(
-    name = "Sum",
-    ext_modules = sum_module,
-    include_dirs = [np.get_include()]
-    )
+
+"""
+--- Step 2 may fail: ---
+
+Traceback (most recent call last):
+  File "setup.py", line 29, in <module>
+    import numpy as np
+  File "/usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/__init__.py", line 170, in <module>
+    from . import add_newdocs
+  File "/usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/add_newdocs.py", line 13, in <module>
+    from numpy.lib import add_newdoc
+  File "/usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/lib/__init__.py", line 8, in <module>
+    from .type_check import *
+  File "/usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/lib/type_check.py", line 11, in <module>
+    import numpy.core.numeric as _nx
+  File "/usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/core/__init__.py", line 6, in <module>
+    from . import multiarray
+ImportError: dlopen(/usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/core/multiarray.so, 2): Symbol not found: _PyUnicodeUCS2_AsASCIIString
+  Referenced from: /usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/core/multiarray.so
+  Expected in: flat namespace
+ in /usr/local/var/pyenv/versions/sum/lib/python2.7/site-packages/numpy/core/multiarray.so
+
+"""
